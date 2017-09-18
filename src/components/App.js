4ea76f41dec868b8010eaa4c0e292fb18d40331a
seunghun <seunghun_kim@cubrid.com>
2017-09-18 10:29:35 +0900

    refactoring Contacts as style of React

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,14 @@
 import React from 'react';
-import Header from './Header';
-import Content from './Content';
-import StateExample from './StateExample';
-import RandomNumber from './RandomNumber';
+import Contacts from './Contacts'
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: Math.round(Math.random() * 100)
-    }
-    this.updateValue = this.updateValue.bind(this);
-  }
-
-  updateValue(randomValue) {
-    this.setState({ value: randomValue });
   }
 
   render() {
-    let name = 'huna';
     return(
       <Contacts />
-    );
-  }
-}
-
-class Contacts extends React.Component {
-  render() {
-    return(
-      <div>
-        <h1>Contacts</h1>
-        <ul>
-          <ContactInfo name="Abet" phone="010-0000-0001" />
-          <ContactInfo name="Betty" phone="010-0000-0002" />
-          <ContactInfo name="Charlie" phone="010-0000-0003" />
-          <ContactInfo name="David" phone="010-0000-0004" />
-        </ul>
-      </div>
-    );
-  }
-}
-
-class ContactInfo extends React.Component {
-  render() {
-    return(
-      <li>{this.props.name} {this.props.phone}</li>
     );
   }
 }
