6f444212b000dc7a87d06478f3e277de38842469
seunghun <seunghun_kim@cubrid.com>
2017-09-18 10:20:30 +0900

    prototype for Contats

--- src/components/App.js
+++ src/components/App.js
@@ -20,24 +20,31 @@ export default class App extends React.Component {
   render() {
     let name = 'huna';
     return(
+      <Contacts />
+    );
+  }
+}
+
+class Contacts extends React.Component {
+  render() {
+    return(
       <div>
-        <Header title={ this.props.headerTitle }/>
-        <Content
-          title={ this.props.contentTitle}
-          body={ this.props.contentBody }
-        />
-        <StateExample />
-        <RandomNumber
-          number={ this.state.value }
-          onUpdate={ this.updateValue }
-        />
+        <h1>Contacts</h1>
+        <ul>
+          <ContactInfo name="Abet" phone="010-0000-0001" />
+          <ContactInfo name="Betty" phone="010-0000-0002" />
+          <ContactInfo name="Charlie" phone="010-0000-0003" />
+          <ContactInfo name="David" phone="010-0000-0004" />
+        </ul>
       </div>
     );
   }
 }
 
-App.defaultProps = {
-  headerTitle: 'Default header',
-  contentTitle: 'Default contentTitle',
-  contentBody: 'Default contentBody'
+class ContactInfo extends React.Component {
+  render() {
+    return(
+      <li>{this.props.name} {this.props.phone}</li>
+    );
+  }
 }
